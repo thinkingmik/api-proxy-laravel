6c09e328c5c3d3a2a46e07312ff6bd589892ebf6
mandreoli <michi.andreoli@gmail.com>
2015-01-17 16:18:32 +0100

    add license

--- src/Andreoli/ApiProxy/ApiProxyServiceProvider.php
+++ src/Andreoli/ApiProxy/ApiProxyServiceProvider.php
@@ -4,7 +4,7 @@
  * @package   andreoli/api-proxy-laravel
  * @author    Michele Andreoli <michi.andreoli[at]gmail.com>
  * @copyright Copyright (c) Michele Andreoli
- * @licence   http://mit-license.org/
+ * @license   http://mit-license.org/
  * @link      https://github.com/mandreoli/api-proxy-laravel
  */
 
